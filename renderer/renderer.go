f9c061fb181ffdcd14f545e872012b2e320593a2
Asapgiri <korcsak.gergely@gmail.com>
2025-10-15 03:19:17 +0200

    Init lib modules

---
+++ renderer/renderer.go
@@ -0,0 +1,137 @@
+package renderer
+
+import (
+	"asapgiri/golib/session"
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+)
+
+var artifact_path string = "artifacts/"
+var html_path string = "html/"
+var base_template_path string = html_path + "base.html"
+
+var file_types = map[string]string {
+    "html": "text",
+    "css":  "text",
+}
+
+func sizeToText(size int) string {
+    const kbDiv = 1024.0
+
+    mb := float64(size) / kbDiv / kbDiv
+    gb := mb / kbDiv
+
+    if gb >= 1.0 {
+        return strconv.FormatFloat(gb, 'f', 2, 64) + " GB"
+    } else {
+        return strconv.FormatFloat(mb, 'f', 2, 64) + " MB"
+    }
+}
+
+var funcMap = template.FuncMap {
+    "inc":      func(i int) int {return i + 1},
+    "dec":      func(i int) int {return i - 1},
+    "size":     sizeToText,
+    "timegt":   func(a time.Time, b time.Time) bool {return b.Compare(a) > 0},
+    "timelt":   func(a time.Time, b time.Time) bool {return b.Compare(a) <= 0},
+    "now":      time.Now,
+    "day":      func() time.Duration {return time.Hour * 24},
+    "tformat":  func(a time.Time) string {return a.Local().Format("2006-01-02 15:04:05")},
+    "shorten":  func(s string, newLen int) string {return s[:newLen] + ".." + s[len(s)-(newLen/2):]},
+}
+
+func ReadArtifact(path string, header http.Header) (string, string) {
+    var dir_path string
+
+    ex, err := os.Executable()
+    if nil != err {
+        panic(err)
+    }
+
+    parts := strings.Split(path, ".")
+    file_type := parts[len(parts)-1]
+    if "html" == file_type {
+        dir_path = filepath.Dir(ex) + "/" + html_path
+    } else {
+        dir_path = filepath.Dir(ex) + "/" + artifact_path
+    }
+
+    file_read, err := os.ReadFile(dir_path + path)
+    if nil != err {
+        not_found, _ := os.ReadFile(filepath.Dir(ex) + "/" + html_path + "not_found.html")
+        return string(not_found), "text"
+    }
+
+    if nil != header {
+        _, file_ok := file_types[file_type]
+        if file_ok {
+            header.Set("Content-Type", file_types[file_type] + "/" + file_type)
+        }
+    }
+
+    return string(file_read), file_type
+}
+
+func SaveArtifact(path string, file multipart.File) error {
+    ex, err := os.Executable()
+    if nil != err {
+        panic(err)
+    }
+
+    dir_path := filepath.Dir(ex) + "/" + artifact_path
+    dstPath := filepath.Join(dir_path, path)
+    dst, err := os.Create(dstPath)
+    if nil != err {
+        return err
+    }
+    defer dst.Close()
+
+    _, err = io.Copy(dst, file)
+    if nil != err {
+        return err
+    }
+
+    return nil
+}
+
+func Render(session session.Sessioner, w http.ResponseWriter, temp string, dto any) {
+    tmp, err := template.ParseFiles(base_template_path)
+    if nil != err {
+        io.WriteString(w, "Templating error!")
+        return
+    }
+
+    session.Main = temp
+    session.Dto = dto
+
+    var tpl bytes.Buffer
+    tmp.Execute(&tpl, session)
+    main, err := template.New("Main").Funcs(funcMap).Parse(tpl.String())
+    if nil != err {
+        io.WriteString(w, "Templating error 2!" + err.Error())
+        return
+    }
+
+    main.Execute(w, session)
+}
+
+// Prerender does not support session if you don't pass it...
+func PreRender(temp string, dto any) string {
+    var tpl bytes.Buffer
+
+    tmp, err := template.New("Dto").Funcs(funcMap).Parse(temp)
+    if nil != err {
+        return err.Error()
+    }
+    tmp.Execute(&tpl, dto)
+
+    return tpl.String()
+}
---
+++ session/session.go
@@ -0,0 +1,75 @@
+package session
+
+import (
+	// "dunakeke/config"
+	// "dunakeke/dictionary"
+	// "dunakeke/logic"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+type Config struct {
+    Title           string
+    SiteTitle       string
+    TitleSeparator  string
+    MaxImgUploadMB  int64
+}
+
+type Auth struct {
+    Id          string
+    Error       string
+    Username    string
+    Name        string
+    Email       string
+    Roles       []string
+    IsAdmin     bool
+    IsMod       bool
+}
+
+type Sessioner struct {
+    Config      Config
+    Auth        Auth
+    Main        string
+    Path        string
+    Dto         any
+    Dictionary  any
+}
+
+//FIXME: Handle fully separately in every function/session!!
+//var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var store = sessions.NewCookieStore([]byte("lsjdglkhdsagjklhads;fjklhasl;kfjs"))
+var sessionName = "dunakeke"
+
+func (session *Sessioner) Authenticate(r *http.Request) {
+    // TODO: Add request aut header
+    real_session, _ := store.Get(r, sessionName)
+    uname, _ := real_session.Values[sessionName].(string)
+
+    session.Auth.Username = uname
+}
+
+func (session *Sessioner) New(w http.ResponseWriter, r *http.Request, uname string) {
+    // FIXME: Store auth headers in database with associated user
+    store.MaxAge(86400)
+    rsess, _ := store.New(r, sessionName)
+
+    rsess.Values[sessionName] = uname
+    rsess.Save(r, w)
+    session.Auth.Username = uname
+}
+
+func (session *Sessioner) Delete(w http.ResponseWriter, r *http.Request) {
+    real_session, _ := store.Get(r, sessionName)
+    real_session.Options.MaxAge = -1
+    real_session.Save(r, w)
+}
+
+func (session *Sessioner) SetError(msg string) {
+    session.Auth.Username = ""
+    session.Auth.Error = msg
+}
+
+func (session *Sessioner) UpdateTitle(config Config, title string) {
+    session.Config.Title = config.Title + config.TitleSeparator + title
+}
